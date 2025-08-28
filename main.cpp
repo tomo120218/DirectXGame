2b47ab32661891f249fb62c0721c765f1c22bc65
tomohiro byoudou <k024g0017@g.neec.ac.jp>
2025-08-28 19:08:06 +0900

    夏休み課題提出

--- main.cpp
+++ main.cpp
@@ -75,7 +75,7 @@ void DrawScene() {
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの初期化
-	KamataEngine::Initialize(L"LE2D");
+	KamataEngine::Initialize(L"LE2D_18_ビョウドウ_トモヒロ_AL3");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
